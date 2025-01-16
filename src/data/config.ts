07c4a7f50117af87557082ef345222145100b37e
Thijs Havinga <info@thijshavinga.nl>
2025-01-16 21:19:48 +0100

    Netlify config

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const SPOTIFY_CLIENT_ID = "9a65a6d51ea84b68ae03bf38202b1754";
-export const REDIRECT_URL = "http://localhost:5173/";
+export const REDIRECT_URL = "https://blind-song-scanner.netlify.app/";
