27e9ed8800a889e7d7a8d23ba0e3cff7fb80c622
Thijs Havinga <info@thijshavinga.nl>
2025-01-30 19:57:51 +0100

    New redirect URL

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const SPOTIFY_CLIENT_ID = "c83265d4ce9040388f64b9485cf06899";
-export const REDIRECT_URL = "https://blind-song-scanner.netlify.app/";
+export const REDIRECT_URL = "https://play.blindsongscanner.com/";
