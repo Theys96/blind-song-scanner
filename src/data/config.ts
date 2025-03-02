cbfc61b877377e784003df1e09263913713363cc
Thijs Havinga <info@thijshavinga.nl>
2025-03-02 11:43:03 +0100

    Added https

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const SPOTIFY_CLIENT_ID = "c83265d4ce9040388f64b9485cf06899";
-export const REDIRECT_URL = "beta.blindsongscanner.com/";
+export const REDIRECT_URL = "https://beta.blindsongscanner.com/";
