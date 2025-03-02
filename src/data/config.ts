d5d1dab4bcd5cde0e221f9a03c85b3875087ccaa
Thijs Havinga <info@thijshavinga.nl>
2025-03-02 11:32:05 +0100

    Beta URL

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const SPOTIFY_CLIENT_ID = "c83265d4ce9040388f64b9485cf06899";
-export const REDIRECT_URL = "http://localhost:4173/";
+export const REDIRECT_URL = "beta.blindsongscanner.com/";
